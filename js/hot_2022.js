4e51dd1a85d375c7772c9ca20a5ed535fadd200c
JE <wjd7dms7@naver.com>
2022-07-14 01:44:22 +0900

    0714-01-44

--- js/hot_2022.js
+++ js/hot_2022.js
@@ -1,15 +1,17 @@
 
-const swiper = new Swiper(".hot_2022_swiper_ind", {
+// const swiper01_1 = new Swiper(".sheet_01 .hot_2022_swiper_ind", {
+const swiper = new Swiper(".sheet_01 .hot_2022_swiper_ind", {
     spaceBetween: 10,   // 공간사이
     slidesPerView: 5,
     freeMode: true,
     watchSlidesProgress: true,
-    autoplay: {
-        delay: 5000,
-    },
+    // autoplay: {
+    //     delay: 5000,
+    // },
 });
+// 
 
-const swiper2 = new Swiper(".hot_2022_swiper", {
+let swiper2 = new Swiper(".sheet_01 .hot_2022_swiper", {
     spaceBetween: 3,
     navigation: {
         nextEl: ".swiper-button-next",
@@ -17,5 +19,34 @@ const swiper2 = new Swiper(".hot_2022_swiper", {
     },
     thumbs: {
         swiper: swiper,
+    },
+    autoplay: {
+        delay: 8000,
+    },
+});
+
+
+const swiper02_1 = new Swiper(".sheet_02 .hot_2022_swiper_ind", {
+    spaceBetween: 10,   // 공간사이
+    slidesPerView: 5,
+    freeMode: true,
+    watchSlidesProgress: true,
+    // autoplay: {
+    //     delay: 5000,
+    // },
+});
+// 
+
+const swiper02_2 = new Swiper(".sheet_02 .hot_2022_swiper", {
+    spaceBetween: 3,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+    autoplay: {
+        delay: 8000,
     },
 });
